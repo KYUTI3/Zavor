6e3d9e2753a1e1449d84c6ac957796139599ac39
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2023-04-16 03:12:58 -0700

    second commit
    r

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  plugins: [require("daisyui")
+],
+  content: ["./src/**/*.{html,js}",
+  "./index.html"],
   theme: {
-    extend: {},
+    extend:  { animation: {
+      'rgb': 'delay-75 infinite',},
+      keyframes: {
+      rgb: {
+          '0%': {
+            'background-color': '#04A3F8'
+          },
+          '25%': {
+            'background-color': '#31AFF3'
+          },
+          '50%': {
+            'background-color': '#3D9ACD'
+          },
+          '75%': {
+            'background-color': '#0D5075'
+          },
+          '100%': {
+            'background-color': '#056BA4'
+          }
+        }
+      },
   },
   plugins:[
     require('daisyui'),
   ], 
-}
-
+    }
+  }
+  
